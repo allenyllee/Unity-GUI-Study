02901a26ebf45c2c8fa05d30ee3f071b76bb5862
Robinerd alex vaio <Robinerd alex vaio@localhost>
2013-10-11 19:31:29 +0200

    tweaked gui. show results.

--- Assets/Profiler.cs
+++ Assets/Profiler.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Profiler : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public static float FrameTimeAverage { get; private set; }
+	public static float FrameTimeStdDev  { get; private set; }
+	
+	private static int frameCounter = 0;
+	private static List<float> deltaTimes = new List<float>();
+	private static bool isFirstFrame = true;
+	private static float lastTime;
+	private static bool finished = false;
+	
+	void Update () {
+		if(finished)
+			return;
+		
+		// Begin measuring after 1 second (to avoid measuring scene loading overhead)
+		if(Time.timeSinceLevelLoad < 1)
+			return;
+		
+		if(isFirstFrame) {
+			lastTime = Time.realtimeSinceStartup;
+			isFirstFrame = false;
+			return;
+		}
+		
+		frameCounter++;
+		float currentTime = Time.realtimeSinceStartup;
+		float elapsedTime = currentTime - lastTime;
+		deltaTimes.Add(elapsedTime);
+		lastTime = currentTime;
+		
+		//stop measuring after 11 seconds to get an actual measurement period of 10 seconds
+		if(Time.timeSinceLevelLoad > 11) {
+			CalculateData();
+			finished = true;
+			Application.LoadLevel ("ShowResults");
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	private static void CalculateData() {
+		FrameTimeAverage = deltaTimes.Average();
+		FrameTimeStdDev  = CalculateStdDev(deltaTimes);
+	}
 	
+	private static float CalculateStdDev(IEnumerable<float> values)
+	{   
+		float result = 0;
+		if (values.Count() > 0) 
+		{      
+			//Compute the Average      
+			float avg = values.Average();
+			
+			//Perform the Sum of squared (value-avg)
+			float sum = values.Sum(d => Mathf.Pow(d - avg, 2));
+			
+			//Put it all together      
+			result = Mathf.Sqrt((sum) / (values.Count()-1));   
+		}   
+		return result;
 	}
 }
---
+++ Assets/Results.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Results : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string text = "";
+		text += "Average frame time:\t"+Profiler.FrameTimeAverage + "\n";
+		text += "Standard Deviation:\t"+Profiler.FrameTimeStdDev + "\n";
+		guiText.text = text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
