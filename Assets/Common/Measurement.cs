1ab403b5985e19b07265c6216fdf65b7963a8f47
Robinerd alex vaio <Robinerd alex vaio@localhost>
2013-10-13 21:59:56 +0200

    Logging data to file with profiler

---
+++ Assets/Common/Measurement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditorInternal;
+using System.Collections;
+
+public class Measurement : MonoBehaviour {
+	
+	private const int SamplesToCollect = 1000;
+	
+	private int startFrameIndex;
+	
+	void Start() {
+		Profiler.logFile = "profileData.log";
+		Profiler.enableBinaryLog = true;
+		Profiler.enabled = true;
+		
+		startFrameIndex = ProfilerDriver.lastFrameIndex;
+	}
+	
+	void Update () {
+		int nrOfSamples = ProfilerDriver.lastFrameIndex - startFrameIndex;
+		
+		//stop measuring after collecting the required number of samples and discarding the initial ones
+		if(nrOfSamples >= SamplesToCollect) {
+			//Profiler.enableBinaryLog = false;
+			//Application.LoadLevel("ShowResults");
+			
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#else
+			Application.Quit();
+			#endif
+		}
+	}
+}
--- Assets/Common/ShowResults.cs
+++ Assets/Common/ShowResults.cs
@@ -1,19 +1,58 @@
 ﻿using UnityEngine;
+using UnityEditorInternal;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ShowResults : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Profiler.AddFramesFromFile("profileData.log");
 		string text = "";
-		text += "Average frame time:\t"+Profiler.FrameTimeAverage + "\n";
-		text += "Standard Deviation:\t"+Profiler.FrameTimeStdDev + "\n";
+		/*text += getFormattedResults("Rendering");
+		text += getFormattedResults("Scripts");
+		text += getFormattedResults("Physics");
+		text += getFormattedResults("VSync");*/
 		guiText.text = text;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	private static string getFormattedResults(string statLabel) {
+		float maxValue;
+		int firstFrame = ProfilerDriver.firstFrameIndex;
+		float[] results = new float[ProfilerDriver.lastFrameIndex - firstFrame];
+		
+		//fetch requested stats into results array
+		ProfilerDriver.GetStatisticsValues (
+			ProfilerDriver.GetStatisticsIdentifier(statLabel),
+			firstFrame, 1.0f, results, out maxValue
+		);
+		
+		// calculate and format average and standard deviation
+		string average = formatTime(results.Average());
+		string stdDev = formatTime(standardDeviation(results));
+		
+		return statLabel + " time:\t" + average + " ± " + stdDev + " ms\n";
+	}
 	
+	private static string formatTime(float nanoSeconds) {
+		return (nanoSeconds / 1000000).ToString("0.0000");
+	}
+	
+	private static float standardDeviation(IEnumerable<float> values)
+	{   
+		float result = 0;
+		if (values.Count() > 0) 
+		{      
+			//Compute the Average      
+			float avg = values.Average();
+			
+			//Perform the Sum of squared (value-avg)
+			float sum = values.Sum(d => Mathf.Pow(d - avg, 2));
+			
+			//Put it all together      
+			result = Mathf.Sqrt((sum) / (values.Count()-1));   
+		}   
+		return result;
 	}
 }
